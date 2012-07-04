f07c0aba7c1d6fb4a16260290f0c5cae8a1a78de
Lino Guglielmo <linogug@aol.com>
2012-07-04 11:12:43 -0400

    Initial Commit

---
+++ Jul5/Jul5AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Jul5AppDelegate.h
+//  Jul5
+//
+//  Created by Lino Guglielmo on 7/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Jul5AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jul5/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Jul5
+//
+//  Created by Lino Guglielmo on 7/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Jul5AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([Jul5AppDelegate class]));
+	}
+}
